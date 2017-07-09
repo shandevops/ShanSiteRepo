3a317bd6bff19f3888c7172882b073176e8abc37
Shanmuga Sundaram Kasi Gnanaveloo <shanmugasundaramkg@outlook.com>
2017-07-09 17:02:57 +0530

    Created my ShanSite Project

---
+++ ShanSite/ShanSite.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShanSite.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ ShanSite/ShanSite.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShanSite.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
